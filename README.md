d875b272442cf6d5662176588c6736699d825116
Diogo R. Ferreira <diogo.ribeiro.ferreira@gmail.com>
2018-05-15 10:05:44 +0100

    common dst_bolo.py for both networks

--- README.md
+++ README.md
@@ -12,36 +12,14 @@ This repository contains neural networks to predict disruptions from bolometer d
 
 ## Instructions
 
-### Time to disruption
-
-- Run `dst_bolo.py` to get the disruption time and bolometer data for every disruptive pulse.
+- Run `dst_bolo.py` to get the disruption time (if applicable) and bolometer data for every pulse.
 
     - This script will only run on a JET computing cluster (e.g. Freia).
     
     - The disruption time is defined as the moment when the current gradient reaches 20 MA/s.
+    
+    - The disruption time for non-disruptive pulses is defined as zero.
 
     - The bolometer data is down-sampled from 5 kHz to 200 Hz (1 sample every 5 ms).
 
     - An output file `dst_bolo.hdf` will be created.
-
-- Run `model_train.py` to train the model.
-
-    - Before running this script, set the number of GPUs and the batch size accordingly.
-
-    - Training will finish automatically once the validation loss no longer improves.
-    
-    - The best model parameters will be saved in `model_weights.hdf`.
-
-- During training, run `plot_loss.py` to see how the loss and validation loss are evolving.
-
-    - The script will indicate the epoch where the minimum validation loss was achieved.
-
-### Disruption probability
-
-- Run `bolo_data.py` to get the bolometer data for every pulse.
-
-    - This script will only run on a JET computing cluster (e.g. Freia).
-    
-    - The bolometer data is down-sampled from 5 kHz to 200 Hz (1 sample every 5 ms).
-
-    - An output file `bolo_data.hdf` will be created.
--- dst_bolo.py
+++ dst_bolo.py
@@ -16,6 +16,9 @@ def get_data(pulse, dda, dtyp):
         raise ValueError
     return data, t
 
+def get_ipla(pulse):
+    return get_data(pulse, 'magn', 'ipla')
+
 def get_bolo(pulse):
     kb5h, kb5h_t = get_data(pulse, 'bolo', 'kb5h')
     kb5v, kb5v_t = get_data(pulse, 'bolo', 'kb5v')
@@ -26,19 +29,44 @@ def get_bolo(pulse):
 
 # -------------------------------------------------------------------------------------
 
+def get_dst(ipla, ipla_t):
+    x0 = ipla_t[:-1]
+    x1 = ipla_t[1:]
+    y0 = ipla[:-1]
+    y1 = ipla[1:]
+    grad = (y1-y0)/(x1-x0)
+    grad_t = (x0+x1)/2.
+    dst = 0.
+    lim = 20e6 # 20 MA/s
+    pos = np.where(grad > lim)[0]
+    if len(pos) > 0:
+        i = pos[0]
+        x0 = grad_t[i-1]
+        x1 = grad_t[i]
+        y0 = grad[i-1]
+        y1 = grad[i]
+        m = (y1-y0)/(x1-x0)
+        b = (y0*x1-y1*x0)/(x1-x0)
+        dst = (lim-b)/m
+    return dst
+
+# -------------------------------------------------------------------------------------
+
 pulse0 = 80128
 pulse1 = 92504
 
-fname = 'bolo_data.hdf'
+fname = 'dst_bolo.hdf'
 print('Writing:', fname)
 f = h5py.File(fname, 'w')
 
 for pulse in range(pulse0, pulse1+1):
     try:
+        ipla, ipla_t = get_data(pulse, 'magn', 'ipla')
+        dst = get_dst(ipla, ipla_t)
         bolo, bolo_t = get_bolo(pulse)
     except ValueError:
         continue
-    
+        
     t = 40.
     i = np.argmin(np.fabs(bolo_t - t))
     bolo = bolo[i:]
@@ -54,14 +82,16 @@ for pulse in range(pulse0, pulse1+1):
     bolo_t = bolo_t[:bolo.shape[0]]
 
     g = f.create_group(str(pulse))
+    g.create_dataset('dst', data=[dst])
     g.create_dataset('bolo', data=bolo)
     g.create_dataset('bolo_t', data=bolo_t)
 
-    print('%10s %10.4f %10.4f %10.4f %10d %10d' % (pulse,
-                                                   bolo_t[0],
-                                                   bolo_t[-1],
-                                                   step,
-                                                   n,
-                                                   bolo_t.shape[0]))
+    print('%10s %10.4f %10.4f %10.4f %10.4f %10d %10d' % (pulse,
+                                                          dst,
+                                                          bolo_t[0],
+                                                          bolo_t[-1],
+                                                          step,
+                                                          n,
+                                                          bolo_t.shape[0]))
 
 f.close()
