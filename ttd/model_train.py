7f662e1bd77b82e49fed81fa07a5bc70eae7a9f6
Diogo R. Ferreira <diogo.ribeiro.ferreira@gmail.com>
2018-05-17 14:01:50 +0100

    added ttd/

--- ttd/model_train.py
+++ ttd/model_train.py
@@ -17,42 +17,31 @@ bolo_t = dict()
 train_pulses = []
 valid_pulses = []
 
-k = 0
 for (i, pulse) in enumerate(f):
-    dst[pulse] = f[pulse]['dst'][0]
-    bolo[pulse] = np.clip(f[pulse]['bolo'][:]/1e6, 0., None)
-    bolo_t[pulse] = f[pulse]['bolo_t'][:]
-    k += 1
-    if k % 10 != 0:
-        train_pulses.append(pulse)
-        print('%10s %10.4f %10.4f %10.4f %10d' % (pulse,
-                                                  dst[pulse],
-                                                  bolo_t[pulse][0],
-                                                  bolo_t[pulse][-1],
-                                                  bolo_t[pulse].shape[0]))
-    else:
-        valid_pulses.append(pulse)
-        print('%10s %10.4f %10.4f %10.4f %10d *' % (pulse,
+    if f[pulse]['dst'][0] > 0.:
+        dst[pulse] = f[pulse]['dst'][0]
+        bolo[pulse] = np.clip(f[pulse]['bolo'][:]/1e6, 0., None)
+        bolo_t[pulse] = f[pulse]['bolo_t'][:]
+        print('\r%10s %10.4f %10.4f %10.4f %10d' % (pulse,
                                                     dst[pulse],
                                                     bolo_t[pulse][0],
                                                     bolo_t[pulse][-1],
-                                                    bolo_t[pulse].shape[0]))
+                                                    bolo_t[pulse].shape[0]), end='')
+        if (i+1) % 10 != 0:
+            train_pulses.append(pulse)
+        else:
+            valid_pulses.append(pulse)
+print()
 
 f.close()
 
-n1 = len(train_pulses)
-n2 = len([pulse for pulse in train_pulses if dst[pulse] > 0.])
-
-n3 = len(valid_pulses)
-n4 = len([pulse for pulse in valid_pulses if dst[pulse] > 0.])
-
-print('train_pulses: %4d' % n1, 'disruptions: %4d (%.2f%%)' % (n2, float(n2)/float(n1)*100.))
-print('valid_pulses: %4d' % n3, 'disruptions: %4d (%.2f%%)' % (n4, float(n4)/float(n3)*100.))
+print('train_pulses:', len(train_pulses))
+print('valid_pulses:', len(valid_pulses))
 
 # ----------------------------------------------------------------------
 
 sample_size = 200
-batch_size = 1024
+batch_size = 2000
 
 def generator(pulses):
     X_batch = []
@@ -65,7 +54,7 @@ def generator(pulses):
         if i < sample_size:
             continue
         x = bolo[pulse][i-sample_size+1:i+1]
-        y = 1. if dst[pulse] > 0. else 0.
+        y = dst[pulse] - bolo_t[pulse][i]
         X_batch.append(x)
         Y_batch.append(y)
         if len(X_batch) >= batch_size:
@@ -87,41 +76,52 @@ set_session(session)
 
 # ----------------------------------------------------------------------
 
-from model import *
-from keras.utils import *
+from keras.models import *
+from keras.layers import *
 from keras.optimizers import *
 
-with tf.device('/cpu:0'):
-    model = create_model(sample_size)
+model = Sequential()
 
-parallel_model = multi_gpu_model(model, gpus=8)
+model.add(Conv1D(32, 5, activation='relu', input_shape=(sample_size, 56)))
+model.add(MaxPooling1D())
+
+model.add(Conv1D(64, 5, activation='relu'))
+model.add(MaxPooling1D())
+
+model.add(Conv1D(128, 5, activation='relu'))
+model.add(MaxPooling1D())
+
+model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
+
+model.add(Dense(1))
+
+model.summary()
 
 opt = Adam(lr=1e-4)
 
-parallel_model.compile(optimizer=opt, loss='binary_crossentropy')
+model.compile(optimizer=opt, loss='mae')
 
 # ----------------------------------------------------------------------
 
 from keras.callbacks import *
-
-f = open('train.log', 'w')
-f.close()
-
-def log_print(s):
-    print(s)
-    f = open('train.log', 'a')
-    f.write(s+'\n')
-    f.flush()
-    f.close()
 
 class MyCallback(Callback):
 
     def __init__(self):
         self.min_val_loss = None
         self.min_val_epoch = None
+        self.f = open('train.log', 'w')
+        self.f.close()
+
+    def log_print(self, s):
+        print(s)
+        self.f = open('train.log', 'a')
+        self.f.write(s+'\n')
+        self.f.flush()
+        self.f.close()
 
     def on_train_begin(self, logs=None):
-        log_print('%-10s %5s %10s %10s' % ('time', 'epoch', 'loss', 'val_loss'))
+        self.log_print('%-10s %5s %10s %10s' % ('time', 'epoch', 'loss', 'val_loss'))
 
     def on_epoch_end(self, epoch, logs=None):
         epoch += 1
@@ -131,26 +131,23 @@ class MyCallback(Callback):
         if (self.min_val_loss == None) or (val_loss < self.min_val_loss):
             self.min_val_loss = val_loss
             self.min_val_epoch = epoch
-            model.save_weights('model_weights.hdf', overwrite=True)
-            log_print('%-10s %5d %10.6f %10.6f *' % (t, epoch, loss, val_loss))
+            self.model.save('model.hdf')
+            self.log_print('%-10s %5d %10.6f %10.6f *' % (t, epoch, loss, val_loss))
         else:
-            log_print('%-10s %5d %10.6f %10.6f' % (t, epoch, loss, val_loss))
+            self.log_print('%-10s %5d %10.6f %10.6f' % (t, epoch, loss, val_loss))
         if epoch >= 2*self.min_val_epoch:
             print('Stop training.')
-            parallel_model.stop_training = True
+            self.model.stop_training = True
 
 # ----------------------------------------------------------------------
 
 try:
-    parallel_model.fit_generator(generator(train_pulses),
-                                 steps_per_epoch=100,
-                                 epochs=10000,
-                                 verbose=0,
-                                 callbacks=[MyCallback()],
-                                 validation_data=generator(valid_pulses),
-                                 validation_steps=10,
-                                 workers=8,
-                                 use_multiprocessing=True,
-                                 max_queue_size=100)
+    model.fit_generator(generator(train_pulses),
+                        steps_per_epoch=100,
+                        epochs=10000,
+                        verbose=0,
+                        callbacks=[MyCallback()],
+                        validation_data=generator(valid_pulses),
+                        validation_steps=10)
 except KeyboardInterrupt:
     print('\nTraining interrupted.')
